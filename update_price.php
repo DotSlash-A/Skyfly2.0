818956ac8988d6e568ef6ba77358b942f33e88dd
V.S.M. Roopa <roopavsm01@gmail.com>
2023-03-07 16:17:11 +0530

    Add files via upload

---
+++ update_price.php
@@ -0,0 +1,31 @@
+<?php
+// Connect to the MySQL database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testdb";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check the connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get the tour ID and new price from the form
+$id = $_POST["id"];
+$new_price = $_POST["new_price"];
+
+// Update the price in the database
+$sql = "UPDATE tours SET price = '$new_price' WHERE id = '$id'";
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+	echo "Price updated successfully";
+} else {
+	echo "Error updating price: " . mysqli_error($conn);
+}
+
+// Close the connection
+mysqli_close($conn);
+?>
